d17e86f010f5d1e1e5bae1d6cf2c7a994ba0ed98
Paulo Burke <pauloepburke@gmail.com>
2026-01-11 20:13:04 -0700

    clean environment

--- lua/config/lsp.lua
+++
@@ -1,4 +0,0 @@
-require("mason").setup()
-require("mason-lspconfig").setup()
--- Autoformat on save
-vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
--- lua/plugins/init.lua
+++
@@ -1,5 +0,0 @@
-return {
-    "echasnovski/mini.icons",
-    "williamboman/mason.nvim",
-    "github/copilot.vim",
-}
