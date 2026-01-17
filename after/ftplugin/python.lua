3f9c7babed5feffbe3d21d82a14245077c807b8c
Paulo Burke <pauloepburke@gmail.com>
2026-01-16 18:12:24 -0700

    add base config with lsp

---
+++ after/ftplugin/python.lua
@@ -0,0 +1,12 @@
+vim.opt_local.expandtab = true
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt_local.softtabstop = 4
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'python' },
+  callback = function()
+		vim.treesitter.start()
+		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+	end,
+})
---
+++ init.lua
@@ -0,0 +1,5 @@
+require("config.lazy")
+require("burke.remap")
+
+vim.o.number = true           -- Enables absolute line numbers
+vim.o.relativenumber = true   -- Enables relative line numbers (current line shows 0)
